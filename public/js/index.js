6f2d286ac7189977d9469cd09d1083de9bfd62d9
Scott Harris <scottsharris@yahoo.com>
2020-07-09 14:58:44 -0700

    Created basic layout for mobile view

--- public/js/index.js
+++ public/js/index.js
@@ -1,28 +1,20 @@
-var now = new Date();
-var year = now.getFullYear();
-var month = now.getMonth() + 1;
-var date = now.getDate();
+const now = new Date();
+const year = now.getFullYear();
+const month = now.getMonth() + 1;
+const date = now.getDate();
 
 
-var data = [{
-    date: year + '-' + month + '-' + (date - 1),
-    value: 'hello'
-}, {
-    date: year + '-' + month + '-' + date,
-    value: '上班'
-}, {
-    date: new Date(year, month - 1, date + 1),
-    value: '吃饭睡觉打豆豆'
-}, {
-    date: '2016-10-31',
-    value: '2016-10-31'
+let data = [{
+    date: '2020-07-20',
+    value: 'Presentation Day'
 }];
 
 // inline
-var $ca = $('#calendar').calendar({
+const $ca = $('#calendar').calendar({
      // width
-     width: window.innerWidth - 50,
+     width: window.innerWidth - 25,
     
      // height, 
-     height: window.innerWidth - 50,
+     height: window.innerWidth - 25,
+     data: data
 });
